7813d9fecb68120b18d1899f993a91e8ce2a5624
Daavid Martinez <david.martinez@ua.es>
2025-11-14 10:58:58 +0100

    change Scene

--- Assets/Scripts/Init_app.cs
+++ Assets/Scripts/Init_app.cs
@@ -2,10 +2,16 @@ using UnityEngine;
 using RovitNacional;
 
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+using TMPro;
 
 public class Init_app : MonoBehaviour
 {
     public GameObject[] NoDestroy;
+
+    public TextMeshProUGUI actividad;
+    public TextMeshProUGUI modo;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,9 +23,34 @@ public class Init_app : MonoBehaviour
             DontDestroyOnLoad(gb);
         }
     }
+    public void OnButtonStartClick()
+    {
+
+        Debug.Log("Actividad " + actividad.text + "\t" + modo.text);
+        switch (actividad.text)
+        {
+            case "Construir Torre": Experimento.Actividad = Activity.BuldingTower; break;
+            case "Go Stop Go": Experimento.Actividad = Activity.GoStopGo; break;
+        }
+
+        switch (modo.text)
+        {
+            case "TEA": Experimento.Modo = Mode.TEA; break;
+            case "Sindrome de Down": Experimento.Modo = Mode.Down; break;
+            case "Altas Capacidades": Experimento.Modo = Mode.AC; break;
+        }
+
+        if (Experimento.Actividad == Activity.GoStopGo)
+            SceneManager.LoadScene("Experiment_1");
+        if (Experimento.Actividad == Activity.BuldingTower)
+            SceneManager.LoadScene("Experiment_2");
+
+    }
 
     public void OnLoadExperiment1()
     {
+        Experimento.Actividad = Activity.GoStopGo;
+       
         SceneManager.LoadScene("Experiment_1");
     }
     public void OnLoadExperiment2()
